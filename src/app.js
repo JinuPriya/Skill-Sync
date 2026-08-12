a061f0c9c1b780b761026c322658b3a57bf3fe43
Jinu Priya <jinupriyabagga2007@gmail.com>
2026-08-12 22:58:12 +0530

    Use stable Vercel domain in CORS

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(cors({
-    origin: ["http://localhost:5173", "https://skill-sync-2cpceu1wu-jinupriyabagga2007-8430s-projects.vercel.app"],
+    origin: ["http://localhost:5173", "https://skill-sync-five-sandy.vercel.app"],
     credentials: true
 }));
 app.use(express.urlencoded({ extended: true }));
