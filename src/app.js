a08a765b386fec485d7a742ad9f3c53e8902c2ac
Jinu Priya <jinupriyabagga2007@gmail.com>
2026-08-12 22:52:35 +0530

    Fix CORS to allow Vercel domain

--- src/app.js
+++ src/app.js
@@ -17,7 +17,8 @@ const app = express()
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: ["http://localhost:5173", "https://skill-sync-2cpceu1wu-jinupriyabagga2007-8430s-projects.vercel.app"],
+    credentials: true
 }));
 app.use(express.urlencoded({ extended: true }));
 
